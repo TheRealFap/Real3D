34287650b19c48afa7f0dd18d5b8bc25b7ee3474
Sebastian <a00sebi@outlook.de>
2016-10-16 16:26:59 +0200

    Just pushing all files

---
+++ Assets/scripts/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	//rotation
+	public float mouseSensivity = 0.0F;
+	private float rotationX = 0.0F;
+	private float rotationY = 0.0F;
+	private float currentRotationX = 0.0F;
+	private float currentRotationY = 0.0F;
+	private float rotationSpeedX = 0.0F;
+	private float rotationSpeedY = 0.0F;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//camera rotation
+		//mouse movement: Rotating on x is like looking up and down, rotating on y is like looking left and right
+		this.rotationX -= Input.GetAxis ("Mouse Y") * this.mouseSensivity;	//Get the mouse movement on the y axis, so rotation on x axis can be done, mouse sensivity can be specified here aswell
+		this.rotationY += Input.GetAxis ("Mouse X") * this.mouseSensivity;	//Get the mouse movement on the x axis, so rotation on the y axis can be done
+		this.rotationX = Mathf.Clamp (rotationX, -90.0F, 90.0F);	//Locks the rotation on the x axis between the very down and up view (-90 and 90)
+		this.currentRotationX = Mathf.SmoothDamp (this.currentRotationX, this.rotationX, ref this.rotationSpeedX, 0.1F);	//smoothly/gradually changes the current rotation x to the new rotation x with rotation speed x
+		this.currentRotationY = Mathf.SmoothDamp (this.currentRotationY, this.rotationY, ref this.rotationSpeedY, 0.1F);	//same here, just with y
+
+		this.transform.rotation = Quaternion.Euler (this.currentRotationX, this.currentRotationY, 0.0F);	//rotates with the calculated values -> note: Z - Rotation will always be set to 0.0F, otherwise rotation would be weird
+
+	}
+
+}
---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	//movement speed
+	public float movementSpeed = 10.0F;
+	public float runningSpeed = 20.0F;
+
+	//ground detection
+	public float groundDetectionLength = 1;
+
+	//camera reference for forward vector
+	public Camera mainCamera;
+
+	//jumping
+	public float jumpHeight = 5.0F;
+	private bool jump = false;
+	public Rigidbody rigidbody;
+
+	//Called at the very beginning of the game
+	void Start() {
+		this.mainCamera = Camera.main;
+
+		this.rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		//movement
+		Vector3 velocity = new Vector3 ();
+		if (Input.GetKey (KeyCode.W)) {
+			Vector3 forward = this.mainCamera.transform.forward;
+			forward.y = 0.0F;
+			velocity += forward * this.movementSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			Vector3 forward = this.mainCamera.transform.forward;
+			forward.y = 0.0F;
+			velocity -= forward * (this.movementSpeed * 0.5F) * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			Vector3 right = this.mainCamera.transform.right;
+			right.y = 0.0F;
+			velocity -= right * (this.movementSpeed * 0.75F) * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			Vector3 right = this.mainCamera.transform.right;
+			right.y = 0.0F;
+			velocity += right * (this.movementSpeed * 0.75F) * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			velocity *= this.runningSpeed / this.movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.Y)) {
+			velocity *= 0.5F;
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			this.jump = true;
+		} else {
+			this.jump = false;
+		}
+		this.transform.localPosition += velocity;
+
+	}
+
+	void FixedUpdate() {
+
+		if (this.jump && this.isObjectGrounded ()) {
+			this.rigidbody.velocity += new Vector3 (0.0F, this.jumpHeight, 0.0F);
+		}
+
+	}
+
+	private bool isObjectGrounded() {
+		Vector3 bottom =  new Vector3(this.transform.position.x, this.transform.position.y - this.transform.localScale.y / 2, this.transform.position.z);
+		Debug.DrawRay (bottom, Vector2.down);
+		if (Physics.Raycast (bottom, Vector3.down, this.groundDetectionLength)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
